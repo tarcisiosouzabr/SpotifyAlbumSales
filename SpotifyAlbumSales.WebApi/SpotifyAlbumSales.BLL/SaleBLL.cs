72c51be674a7667a5a7a817c8e16fdf3c39cfad7
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2019-01-28 17:44:42 -0200

    Sale

---
+++ SpotifyAlbumSales.WebApi/SpotifyAlbumSales.BLL/SaleBLL.cs
@@ -0,0 +1,28 @@
+﻿using SpotifyAlbumSales.BLL.Infra;
+using SpotifyAlbumSales.DAL.Infra.Repositories;
+using SpotifyAlbumSales.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyAlbumSales.BLL
+{
+    public class SaleBLL : ISaleBLL
+    {
+        private ISaleRepository _saleRepository;
+        public SaleBLL(ISaleRepository saleRepository)
+        {
+            _saleRepository = saleRepository;
+        }
+        public Task<List<Sale>> GetAsync(DateTime? initialDate, DateTime? finalDate, int page, int pageSize)
+        {
+            return _saleRepository.GetAsync(initialDate, finalDate, page, pageSize);
+        }
+
+        public Task<Sale> GetByIdAsync(Guid id)
+        {
+            return _saleRepository.GetByIdAsync(id);
+        }
+    }
+}
---
+++ SpotifyAlbumSales.WebApi/SpotifyAlbumSales.DAL.Infra/Repositories/ISaleRepository.cs
@@ -0,0 +1,14 @@
+﻿using SpotifyAlbumSales.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyAlbumSales.DAL.Infra.Repositories
+{
+    public interface ISaleRepository
+    {
+        Task<List<Sale>> GetAsync(DateTime? initialDate, DateTime? finalDate, int page, int pageSize);
+        Task<Sale> GetByIdAsync(Guid id);
+    }
+}
