ea9ed9dc54828cb1e0b5b873042bd9e0b5253a12
Tarcísio Vitor de Souza <tarcisiovity@hotmail.com>
2019-01-30 07:57:47 -0200

    spotify integration Start

--- SpotifyAlbumSales.WebApi/SpotifyAlbumSales.Api/Controllers/AlbumController.cs
+++ SpotifyAlbumSales.WebApi/SpotifyAlbumSales.Api/Controllers/AlbumController.cs
@@ -1,7 +1,11 @@
 ﻿using EasySoccer.Api.Controllers.Base;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SpotifyAlbumSales.Api.Infra;
 using SpotifyAlbumSales.Api.UoWs;
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace SpotifyAlbumSales.Api.Controllers
@@ -10,9 +14,14 @@ namespace SpotifyAlbumSales.Api.Controllers
     public class AlbumController : ApiBaseController
     {
         private AlbumUoW _uow;
-        public AlbumController(AlbumUoW uow) : base(uow)
+        private IHttpClientFactory _httpClientFactory;
+        private IExternalData _externalData;
+
+        public AlbumController(AlbumUoW uow, [FromServices]IHttpClientFactory httpClientFactory, IExternalData externalData) : base(uow)
         {
             _uow = uow;
+            _httpClientFactory = httpClientFactory;
+            _externalData = externalData;
         }
 
         [Route("api/album/get"), HttpGet]
@@ -25,6 +34,15 @@ namespace SpotifyAlbumSales.Api.Controllers
         public async Task<IActionResult> GetByIdAsync([FromQuery]Guid albumId)
         {
             return Ok(await _uow.AlbumBLL.GetByIdAsync(albumId));
+        }
+
+        [Route("api/album/filldata"), HttpPost]
+        public async Task<IActionResult> PostFillDataAsync()
+        {
+            var authClient = _httpClientFactory.CreateClient("SpotifyHttpClientAuth");
+            var authResponse = await _externalData.Authentication(authClient);
+            var queryClient = _httpClientFactory.CreateClient("SpotifyHttpClientSearch");
+            return Ok();
         }
     }
 }
--- SpotifyAlbumSales.WebApi/SpotifyAlbumSales.Api/Startup.cs
+++ SpotifyAlbumSales.WebApi/SpotifyAlbumSales.Api/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SpotifyAlbumSales.Api.Controllers;
+using SpotifyAlbumSales.Api.Infra;
 using SpotifyAlbumSales.Api.UoWs;
 using SpotifyAlbumSales.BLL;
 using SpotifyAlbumSales.BLL.Infra;
@@ -10,6 +12,8 @@ using SpotifyAlbumSales.DAL;
 using SpotifyAlbumSales.DAL.Infra;
 using SpotifyAlbumSales.DAL.Infra.Repositories;
 using SpotifyAlbumSales.DAL.Repositories;
+using System;
+using System.Net.Http.Headers;
 
 namespace SpotifyAlbumSales.Api
 {
@@ -26,6 +30,7 @@ namespace SpotifyAlbumSales.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddJsonOptions(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddSingleton(Configuration);
             services.AddDbContext<ISpotifyAlbumSalesDbContext, SpotifyAlbumSalesDbContext>(
                         x => x.UseSqlServer(Configuration.GetConnectionString("SpotifyAlbumSalesDbContext")));
 
@@ -46,7 +51,28 @@ namespace SpotifyAlbumSales.Api
             services.AddScoped<ISaleRepository, SaleRepository>();
             #endregion
 
+            services.AddScoped<IExternalData, ExternalData>();
+
             services.AddSwaggerDocument();
+
+            services.AddHttpClient("SpotifyHttpClientAuth", client =>
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));    
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(Configuration.GetSection("SpotifyApi:ClientId").Value + ":" + Configuration.GetSection("SpotifyApi:ClientSecret").Value);
+                var clientIdSecretBase64 = System.Convert.ToBase64String(plainTextBytes);
+                client.DefaultRequestHeaders.Add("Authorization", "Basic " + clientIdSecretBase64);
+                client.BaseAddress = new Uri(Configuration.GetSection("SpotifyApi:AuthUrl").Value);
+            });
+
+            services.AddHttpClient("SpotifyHttpClientSearch", client =>
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                client.BaseAddress = new Uri(Configuration.GetSection("SpotifyApi:SearchUrl").Value);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
